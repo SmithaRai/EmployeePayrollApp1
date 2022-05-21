41ff548897ab5a87039a4a4c8d10c53344931022
unknown <SmithaRai.com>
2022-05-21 19:01:18 +0530

    Added : added employee roll app

---
+++ src/test/java/com/bridgelabz/employeepayrollapp1/EmployeePayrollApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollApp1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
